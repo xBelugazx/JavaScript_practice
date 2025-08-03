9475fe27a657047f55fedb7804ac00488021a69d
ningning goat <bskdh98@gmail.com>
2025-08-03 01:04:20 -0500

    Update getter and setter.js

--- class_practice/getter and setter.js
+++ class_practice/getter and setter.js
@@ -25,3 +25,25 @@ const yuJin = new IdolModel('안유진', 2003);
  * 하나의 변수, 오브젝트 키 값처럼 사용함.
  */
 console.log(yuJin.nameAndYear); // 안유진-2003
+
+class IdolModel{
+    name;
+    year;
+
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+
+    // 파라미터는 무조건 1개를 받음
+    set name/*보통 바꾸고 싶은 프로퍼티 이름과 겹치게 함*/(name) {
+        this.name = name;
+        // name값을 받아서 현재 인스턴스 name에 저장함
+    }
+}
+
+const yuJin = new IdolModel('안유진', 2003);
+
+// setter 사용방법
+yuJin.name = '장원영';
+console.log(yuJin);
