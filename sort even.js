39a4046359481da8f7fc1878f22c4ba89c47c52b
ningning goat <bskdh98@gmail.com>
2025-08-20 21:02:05 -0500

    Update sort even.js

--- sort even.js
+++ sort even.js
@@ -1,4 +1,4 @@
-// 1부터 50까지 숫자 중 짝수만 출력
+// 1부터 50까지 숫자 중 짝수만 출력(you can output even numbers)
 
 // for문으로 1~50 돌리면서
 
@@ -11,3 +11,4 @@ for(let i = 1; i <= 50; i++) {
 }
 
 // This file contains an example that prints only even numbers from 1 to 50.
+
